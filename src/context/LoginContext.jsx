bf2ea8c429eb493dd38778255ff6979b57b36b22
AndreasNN1 <111448219+AndreasNN-1@users.noreply.github.com>
2025-03-17 11:58:05 +0100

    Update LoginContext.jsx

--- src/context/LoginContext.jsx
+++ src/context/LoginContext.jsx
@@ -1,36 +1,40 @@
-import { createContext, useState, useEffect } from 'react';
+import { createContext, useState, useEffect } from "react";
 
 export const LoginContext = createContext();
 
 const LoginContextProvider = (props) => {
-    const [user, setUser] = useState(null);
+  const [user, setUser] = useState(null);
 
-    // check log in
-    useEffect(() => {
-        if (storedUser === "admin@abc.dk") {
-            setUser(storedUser);
-        } else {
-           setUser(null);
-        }
-    }, []);
+  // check log in
+  useEffect(() => {
+    const storedUser = localStorage.getItem("user");
+    if (storedUser === "admin@abc.dk") {
+      setUser(storedUser);
+    } else {
+      localStorage.removeItem("user");
+      setUser(null);
+    }
+  }, []);
 
-    // Log in
-    const signIn = async (inputIdentity, inputPassword) => {
-        if (inputIdentity === "admin@abc.dk" && inputPassword === "abc123") {
-            setUser("admin@abc.dk");
-        }
-    };
+  // Log in
+  const signIn = async (inputIdentity, inputPassword) => {
+    if (inputIdentity === "admin@abc.dk" && inputPassword === "abc123") {
+      localStorage.setItem("user", inputIdentity);
+      setUser("admin@abc.dk");
+    }
+  };
 
-    // Log out
-    const signOut = () => {
-        setUser(null);
-    };
+  // Log out
+  const signOut = () => {
+    localStorage.removeItem("user");
+    setUser(null);
+  };
 
-    return (
-        <LoginContext.Provider value={{ signIn, signOut, user }}>
-            {props.children}
-        </LoginContext.Provider>
-    );
+  return (
+    <LoginContext.Provider value={{ signIn, signOut, user }}>
+      {props.children}
+    </LoginContext.Provider>
+  );
 };
 
 export default LoginContextProvider;
