48a48f973d75b3d6b30ba41741d76551c1d0ef08
AndreasNN1 <111448219+AndreasNN-1@users.noreply.github.com>
2025-03-17 11:24:41 +0100

    Update useRequstData.jsx

--- src/hooks/useRequstData.jsx
+++ src/hooks/useRequstData.jsx
@@ -1,56 +1,51 @@
 import axios from 'axios'
-import React from 'react'
 import { useState } from 'react'
 
 const useRequstData = () => {
 
-    const [ isLoading, setIsLoading ] = useState( false )
-    
-    const [ data, setData ] = useState( null )
-    
-    const [ error, seterror ] = useState( false )
+    const [isLoading, setIsLoading] = useState(false)
+    const [data, setData] = useState(null)
+    const [error, setError] = useState(false)
 
-    const makeRequest = async ( url, method = "GET" ) => {
-
-        let response
-        setIsLoading( true )
+    const makeRequest = async (url, method = "GET", bodydata = null, headers = null, params = null) => {
+        setIsLoading(true);
 
         try {
-
-            response = await axios.get(url)
-
-            if( response && response.data !== undefined ) {
-
-                setData( response.data )
-                seterror( false )
-
-            } else {
-
-                seterror( true )
-                setData( null )
-                throw new Error( "FEJL: L" )
-
+            let response;
+            const config = {
+                headers,
+                params,
+              };
+            switch (method) {
+                case "GET":
+                    response = await axios.get(url, config);
+                    break;
+                case "POST":
+                    response = await axios.post(url, bodydata, config);
+                    break;
+                case "DELETE":
+                    response = await axios.delete(url, config);
+                    break;
+                case "PUT":
+                    response = await axios.put(url, bodydata, config);
+                    break;
+                case "PATCH":
+                    response = await axios.patch(url, bodydata, config);
+                    break;
+                default:
+                    throw new Error("Invalid method. GET, POST, PUT, DELETE or PATCH was expected");
             }
-
+            setData(response.data);
+            setError(null);
         } catch (error) {
-
-            seterror( true )
-            setData( null )
-            console.log( error )
-
+            setError("There was an error: " + error.message);
+            setData(null);
         } finally {
-
-         setIsLoading( false )
-         console.log("SLUT")
-
+            setIsLoading(false);
         }
+    };
 
-    }
-
-
-
-
-    return{ makeRequest, isLoading, data, error }
+    return { makeRequest, isLoading, data, error };
 }
 
 export default useRequstData
